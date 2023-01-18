e7ed65d4ed8d1b0c9e1cf56bb40859b60f818cf7
yogesh.prajapati <yogesh.prajapati@corptec.com.au>
2023-01-18 12:12:00 +0530

    inital commit

--- src/main/java/com/sys/hs/customer/controller/OpportunityController.java
+++ src/main/java/com/sys/hs/customer/controller/OpportunityController.java
@@ -1,38 +1,35 @@
 package com.sys.hs.customer.controller;
 
-import javax.annotation.security.RolesAllowed;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.sys.hs.customer.entity.Deal;
 import com.sys.hs.customer.entity.Opportunity;
 import com.sys.hs.customer.service.IOpportunityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/deal")
 public class OpportunityController {
 
-	@Autowired
-	private IOpportunityService opportunityService;
+    @Autowired
+    private IOpportunityService opportunityService;
 
-	@GetMapping
-	Deal getDealById(@RequestParam("id") String id) {
-		return opportunityService.getDealById(Long.parseLong(id));
-	}
+    @GetMapping
+    Deal getDealById(@RequestParam("id") String id) {
+        return opportunityService.getDealById(Long.parseLong(id));
+    }
 
-	@PostMapping
-	Opportunity saveDeal(@RequestBody Opportunity opportunity) {
-		return opportunityService.save(opportunity);
-	}
+    @GetMapping
+    String hello() {
+        return "Hello World";
+    }
 
-	@PostMapping("/update")
-	Opportunity updateDeal(@RequestBody Opportunity opportunity) {
-		return opportunityService.update(opportunity);
-	}
+    @PostMapping
+    Opportunity saveDeal(@RequestBody Opportunity opportunity) {
+        return opportunityService.save(opportunity);
+    }
+
+    @PostMapping("/update")
+    Opportunity updateDeal(@RequestBody Opportunity opportunity) {
+        return opportunityService.update(opportunity);
+    }
 }
